460ecc6ac07877f1ece6f09ee72b57cbf64a4d84
s01va <jinn0525@gmail.com>
2020-12-23 09:26:52 +0900

    linkedlist

--- data_structure/chap07_linkedlist.py
+++ data_structure/chap07_linkedlist.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-# Node 구현
 """
+# Node 구현
 class Node:
 	def __init__(self, data):
 		self.data = data
 		self.next = None
-"""
+
 class Node:
 	def __init__(self, data, next = None):
 		self.data = data
@@ -40,3 +40,96 @@ while node.next:
 	print(node.data)
 	node = node.next
 print(node.data)
+
+# Node를 중간에 끼워넣기
+
+node3 = Node(1.5)
+
+node = head
+search = True
+while search:
+	if node.data == 1:
+		search = False
+	else:
+		node = node.next
+
+node_next = node.next
+node.next = node3
+node3.next = node_next
+
+node = head
+while node.next:
+	print(node.data)
+	node = node.next
+print(node.data)
+"""
+###################################################
+
+# 파이썬 객체지향 프로그래밍으로 Linked list 구현
+
+class Node:
+	def __init__(self, data, next=None):
+		self.data = data
+		self.next = next
+
+class NodeMgmt:
+	def __init__(self, data):
+		self.head = Node(data)
+
+	def add(self, data):
+		if self.head == '':
+			self.head = Node(data)
+		else :
+			node = self.head
+			while node.next:
+				node = node.next
+			node.next = Node(data)
+
+	def desc(self):
+		node = self.head
+		while node:
+			print(node.data)
+			node = node.next
+
+	def delete(self, data):
+		if self.head == '':
+			print("해당 값을 가진 노드 없음.")
+			return
+
+		if self.head.data == data:	# self.head를 삭제해야 할 경우
+			temp = self.head
+			self.head = self.head.next
+			del temp
+		else:	# self.head가 아닌 노드를 삭제할 경우
+			node = self.head
+			while node.next:
+				if node.next.data == data:
+					temp = node.next
+					node.next = node.next.next
+					del temp
+					return
+				else:
+					node = node.next
+
+	def search_node(self, data):
+		node = self.head
+		while node:
+			if node.data == data:
+				return node
+			else:
+				node = node.next
+
+linkedlist1 = NodeMgmt(0)
+linkedlist1.desc()
+
+for data in range(1,10):
+	linkedlist1.add(data)
+linkedlist1.desc()
+
+print(linkedlist1.head)
+
+linkedlist1.delete(0)
+print(linkedlist1.head)
+linkedlist1.delete(4)
+linkedlist1.desc()
+
