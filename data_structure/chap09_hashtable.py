279990fcc0aeb13f5ecd86ec0d12a74c4f00aad7
s01va <jinn0525@gmail.com>
2020-12-24 17:15:03 +0900

    hashtable example

--- data_structure/chap09_hashtable.py
+++ data_structure/chap09_hashtable.py
@@ -160,3 +160,81 @@ print (hash('dc') % 8)
 save_data('dk', '01200123123')
 save_data('da', '3333333333')
 read_data('dc')
+
+# 빈번한 해쉬 충돌 개선 -> 테이블 저장공간 늘리기
+
+#############################################
+
+# sha-1
+
+import hashlib
+
+data = 'test'.encode()
+hash_object = hashlib.sha1()
+hash_object.update(data)
+hex_dig = hash_object.hexdigest()
+print(hex_dig)
+
+#############################################
+
+# sha-256
+
+import hashlib
+
+data = 'test'.encode()
+hash_object = hashlib.sha256()
+hash_object.update(data)
+hex_dig = hash_object.hexdigest()
+print(hex_dig)
+
+#############################################
+
+# 연습4: 연습2의 Chaining 기법을 적용한 해쉬 테이블 코드에 키 생성 함수를 sha256 해쉬 알고리즘을 사용하도록 변경해보기
+# 1. 해쉬 함수: key % 8
+# 2. 해쉬 키 생성: hash(data)
+
+# 앞서 구현했던 Linear Probing을 수정
+
+import hashlib
+
+hash_table = list(0 for i in range(8))
+
+def get_key(data):
+	hash_object = hashlib.sha256()
+	hash_object.update(data.encode())
+	hex_dig = hash_object.hexdigest()
+	return int(hex_dig, 16)
+
+def hash_function(key):
+	return key % 8
+
+def save_data(data, value):
+	index_key = get_key(data)
+	hash_address = hash_function(index_key)
+	if hash_table[hash_address] != 0:
+		for index in range(hash_address, len(hash_table )):
+			if hash_table[index] == 0:
+				hash_table[index] = [hash_address, value]
+				return
+			elif hash_table[index][0] == index_key:
+				hash_table[index][1] = value
+				return
+	else:
+		hash_table[hash_address] = [index_key, value]
+
+def read_data(data):
+	index_key = get_key(data)
+	hash_address = hash_function(index_key)
+
+	if hash_table[hash_address] != 0:
+		for index in range(hash_address, len(hash_table)):
+			if hash_table[index] == 0:
+				return None
+			elif hash_table[index][0] == index_key:
+				return hash_table[index][1]
+	else:
+		return None
+
+print (get_key('db') % 8)
+print (get_key('da') % 8)
+print (get_key('dh') % 8)
