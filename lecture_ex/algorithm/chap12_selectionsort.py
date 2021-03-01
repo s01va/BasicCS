5f5234a05482d9d4b53cb7f5736f64754d036912
s01va <jinn0525@gmail.com>
2021-03-01 16:02:40 +0900

    selection_sort

---
+++ lecture_ex/algorithm/chap12_selectionsort.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def selection_sort(data):
+	for stand in range(len(data) - 1):
+		lowest = stand
+		for index in range(stand+1, len(data)):
+			if data[lowest] > data[index]:
+				lowest = index
+			data[lowest], data[stand] = data[stand], data[lowest]
+	return data
+
+#######################################
+
+# 사용!
+
+import random
+data_list = random.sample(range(100, 10))
+print(selection_sort(data_list))
