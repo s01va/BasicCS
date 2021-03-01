87bab6fdefc9e9af3eac2e36604459eeee171e45
s01va <jinn0525@gmail.com>
2021-03-01 16:02:11 +0900

    insertion_sort

---
+++ lecture_ex/algorithm/chap12_insertionsort.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def insertion_sort(data):
+	for index in range(len(data)-1):
+		for index2 in range(index+1, 0, -1):
+			if data[index2] < data[index2-1]:
+				data[index2], data[index2-1] = data[index2-1], data[index2]
+			else:
+				break
+	return data
+
+#######################################
+
+# 사용!
+
+import random
+
+data_list = random.sample(range(100), 50)
+print(insertion_sort(data_list))
